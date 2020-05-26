024657a57a363f0b7f80bb91e0822dfa1e4e4cdf
Konstantin <konpichugin@yandex.ru>
2020-05-26 10:25:32 +0200

    create function diff

---
+++ src/diff.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Differ\Differ\diff;
+
+
+function genDiff($pathToFile1, $pathToFile2)
+{
+
+    $before = json_decode(file_get_contents($pathToFile1), true);
+    $after = json_decode(file_get_contents($pathToFile2), true);
+
+
+$result = [];
+
+foreach ($before as $keyBef => $valueBef) {
+    foreach ($after as $keyAft => $valueAft) {
+        if ($keyBef === $keyAft and $before[$keyBef] === $after[$keyAft]) {
+            $result[] = $keyBef . ': ' . $valueBef;
+        } elseif ($keyBef === $keyAft and $before[$keyBef] !== $after[$keyAft]) {
+            $result[] = '+' . ' ' . $keyAft . ': ' . $valueAft;
+            $result[] = '-' . ' ' . $keyBef . ': ' . $valueBef;
+        } elseif(isset($before[$keyBef]) and empty($after[$keyBef])) {
+            $result[] = '-' . ' ' . $keyBef . ':' . ' ' . $valueBef;
+        } elseif (empty($before[$keyAft]) and isset($after[$keyAft])) {
+            $result[] = '+' . ' ' . $keyAft . ':' . $valueAft;
+        }
+
+    }
+}
+    $outputArray = array_unique($result);
+    $outputString =  implode("\n", $outputArray);
+    return $outputString . "\n";
+}
