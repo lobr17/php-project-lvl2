07ef3a885d3f9ddd52657a69280f29afe8b2dc07
Konstantin <konpichugin@yandex.ru>
2020-06-18 20:09:33 +0200

    positive test

--- tests/diffTest.php
+++ tests/diffTest.php
@@ -11,11 +11,11 @@ class DiffTest extends TestCase
     public function testDiff()
     {
         $actual = genDiff('before.json', 'after.json');
-        $expected = "+ verbose:1
-host: hexlet.io
+	$expected = "host: hexlet.io
 + timeout: 20
 - timeout: 50
 - proxy: 123.234.53.22
++ verbose: 1
 ";
         $this->assertEquals($expected, $actual);
     }
