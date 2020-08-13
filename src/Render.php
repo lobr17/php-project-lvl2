55964b6fd16526481c64b87246c286ff8ac90543
Konstantin <29775482+lobr17@users.noreply.github.com>
2020-08-13 17:52:50 +0300

    Update Render.php

--- src/Render.php
+++ src/Render.php
@@ -9,30 +9,54 @@ namespace Differ\Differ\Render;
 
 function render($array)
 {
+    //Делаем читаемый массив.
     $result = array_map(function ($child) {
         if ($child['type'] === '-') {
-            return "{$child['type']}, {$child['name']}, {$child['children']}";
+            if (!is_array($child['children'])) {
+                return "{$child['type']}, {$child['name']}, {$child['children']}";
+            } elseif (is_array($child['children'])) {
+                return "{$child['type']}, {$child['name']}, " . "{" . "\n                 " . implode(array_keys($child['children'])) . ": " . implode($child['children']) . "\n" . "}";
+            }
         } elseif ($child['type'] === '+') {
-            return "{$child['type']}, {$child['name']}, {$child['children']}";
+            if (!is_array($child['children'])) {
+                return "{$child['type']}, {$child['name']}, {$child['children']}";
+            } elseif (is_array($child['children'])) {
+                return "{$child['type']}, {$child['name']}, " . "{" . "\n                 " . implode(array_keys($child['children'])) . ": " . implode($child['children']) . "\n" . "}";
+            }
         } elseif ($child['type'] === 'has not changed') {
-            return "{$child['type']}, {$child['name']}, {$child['children']}";
+            if (!is_array($child['children'])) {
+                return "{$child['type']}, {$child['name']}, {$child['children']}";
+            } elseif (is_array($child['children'])) {
+                return "{$child['type']}, {$child['name']}, " . "{" . "\n                 " . implode(array_keys($child['children'])) . ": " . implode($child['children']) . "\n" . "}";
+            }
         } elseif ($child['type'] === 'changed') {
-            return "{$child['type']}, {$child['name']}, {$child['children']}";
+            if (!is_array($child['children'])) {
+                return "{$child['type']}, {$child['name']}, {$child['children']}";
+            } elseif (is_array($child['children'])) {
+                return "{$child['type']}, {$child['name']}, " . "{" . "\n                 " . implode(array_keys($child['children'])) . ": " . implode($child['children']) . "\n" . "}";
+            }
         } elseif ($child['type'] === 'nested') {
             unset($child['type']);
-         //   $key = array_keys($child);
-           // print_r($key);
 
             $result = array_map(function ($subChild) use ($child) {
                 if (is_array($subChild)) {
                     return render($subChild);
                 }
             }, $child);
-
             return $result;
         }
     }, $array);
 
-    return $result;
+    //Массив переводим в строку.
+    $resultRender = array_reduce($result, function ($acc, $child) {
+            if (is_array($child)) {
+                $acc .= implode(array_keys($child)) . ' {' . "\n" . implode($child) . '}' . "\n";
+                return $acc;
+            }
+            $acc .= $child . "\n";
+            return $acc;
+    }, '');
+
+    return $resultRender;
 }
 
