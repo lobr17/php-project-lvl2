8fbd110bc774bb799ce34c75445d29daa5445090
Konstantin <konpichugin@yandex.ru>
2020-10-14 16:10:06 +0300

    correct tests

--- tests/genDiffTest.php
+++ tests/genDiffTest.php
@@ -24,52 +24,51 @@ class DiffTest1 extends TestCase
 
         $expected = <<<DOC
 {
-    common: { 
+    common: {
       + follow: false
         setting1: Value 1
       - setting2: 200
-      - setting3: true
+      - setting3: true 
       + setting3: {
-                key:  value 
-        }
+          key: value
+      }
       + setting4: blah blah
       + setting5: {
-                key5:  value5 
-        }
-       setting6: { 
-          doge: { 
-            - wow: too much
-            + wow: so much
-         }
-           key: value
-         + ops: vops
+          key5: value5
       }
-   }
-    group1: { 
-      - baz: bas
+        setting6: {
+            doge: {
+              - wow: too much 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas 
       + baz: bars
         foo: bar
       - nest: {
-                key:  value 
-        }
+          key: value
+      } 
       + nest: str
-   }
-   - group2: {
-          abc:  12345 
-          deep:  {
-              id:  45 
-       } 
-     }
-   + group3: {
-          fee:  100500 
-          deep:  {
-              id:  {
-                  number:  45 
-         } 
-       } 
-     }
+    }
+  - group2: {
+      abc: 12345
+      deep: {
+          id: 45
+      }
+  }
+  + group3: {
+      fee: 100500
+      deep: {
+          id: {
+              number: 45
+          }
+      }
+  }
 }
-
 DOC;
         $this->assertEquals($expected, $actual);
     }
